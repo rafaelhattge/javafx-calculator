0422b3a26b76d3fc62961e20c62521ebd9012837
Rafael Ivan Hattge <rafaelhattge@gmail.com>
2018-12-10 23:21:36 -0200

    Test1

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -11,75 +11,55 @@ public class Calculator {
     private double number;
     private double result;
 
-    public String getDigit() {
-        return digit;
-    }
-
-    public void setDigit(String digit) {
-        this.digit = digit;
-    }
-
+    //Getters and Setters
+    public String getDigit() { return digit; }
+    public void setDigit(String digit) { this.digit = digit; }
     public String getMainPanel() {
         return mainPanel;
     }
-
-    public void setMainPanel(String mainPanel) {
-        this.mainPanel = mainPanel;
-    }
-
+    public void setMainPanel(String mainPanel) { this.mainPanel = mainPanel; }
     public String getSecondPanel() {
         return secondPanel;
     }
-
     public void setSecondPanel(String secondPanel) {
         this.secondPanel = secondPanel;
     }
-
     public char getOperator() {
         return operator;
     }
-
     public void setOperator(char operator) {
         this.operator = operator;
     }
-
     public double getNumerator() {
         return numerator;
     }
-
     public void setNumerator(double numerator) {
         this.numerator = numerator;
     }
-
     public double getDenominator() {
         return denominator;
     }
-
     public void setDenominator(double denominator) {
         this.denominator = denominator;
     }
-
     public double getNumber() {
         return number;
     }
-
     public void setNumber(double number) {
         this.number = number;
     }
-
     public double getResult() {
         return result;
     }
-
     public void setResult(double result) {
         this.result = result;
     }
 
-    public void addDigit(String digit){
-        if(mainPanel.equals("0")) {
+    //Other methods
+    public void addDigit(String digit) {
+        if (mainPanel.equals("0")) {
             setMainPanel(digit);
-        }
-        else{
+        } else {
             setMainPanel(getMainPanel() + digit);
         }
     }
